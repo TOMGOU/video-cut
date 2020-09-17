741c441a341881150f754cd8a4ef9a05f9010391
tommy.tang <tommy.tang@lixinchuxing.com>
2020-09-17 17:29:10 +0800

    fix: 添加更多自定义参数

--- nba.py
+++ nba.py
@@ -1,19 +1,20 @@
 import os
 
-inputVideo = r'nba\lbj.mp4'
-outputVideo = r'nba\finished\lbjvideo.mp4'
-startTime = 65
+inputVideo = r'nba/top5.mp4'
+outputVideo = r'nba/finished/top1.mp4'
+startTime = 10
 videoDuration = 20
 # cutCmdStr = f'ffmpeg -y -ss{ startTime } -i { inputVideo } -to { videoDuration } -c copy { outputVideo }'
 # os.popen(cutCmdStr)
 
 bgColor = '#6f00a7'
 fontColor = '#ffff00'
-clip = 'crop=iw-400:ih-150:200:0'
-padStr = f'pad=iw:ih+1000:0:500:{ bgColor }'
-text1 = f'drawtext="fontfile=gb.ttf:text=湖人大胜马赛克:x=(w-tw)/2:y=200:fontcolor={ fontColor }:fontsize=60"'
-text2 = f'drawtext="fontfile=gb.ttf:text=同情威少5分钟:x=(w-tw)/2:y=300:fontcolor={ fontColor }:fontsize=60"'
-text3 = f'drawtext="fontfile=gb.ttf:text=湖人总冠军，没人有异议吧？:x=(w-tw)/2:y=400:fontcolor={ fontColor }:fontsize=60"'
-cmdStr = f'ffmpeg -y -ss { startTime } -i  { inputVideo } -to { videoDuration } -vf [in]{ clip },{ padStr },{ text1 },{ text2 },{ text3 }[out] { outputVideo }'
+clip = 'crop=iw-474:ih:937:0'
+padStr = f'pad=iw:16*iw/9:0:"(16*iw/9-ih)/2":{ bgColor }'
+text1 = f'drawtext="fontfile=es.ttf:text=Kawhi wanted a "DOC":x=(w-tw)/2:y=200:fontcolor={ fontColor }:fontsize=60"'
+text2 = f'drawtext="fontfile=es.ttf:text=But what needs is a "NURSE":x=(w-tw)/2:y=300:fontcolor={ fontColor }:fontsize=60"'
+text3 = f'drawtext="fontfile=es.ttf:text=LAKERS VS NUGGETS:x=(w-tw)/2:y=400:fontcolor={ fontColor }:fontsize=60"'
+# cmdStr = f'ffmpeg -y -ss { startTime } -i  { inputVideo } -to { videoDuration } -vf [in]{ clip },{ padStr },{ text1 },{ text2 },{ text3 }[out] { outputVideo }'
+cmdStr = f'ffmpeg -y -ss { startTime } -i  { inputVideo } -to { videoDuration } -vf [in]{ clip },{ padStr }[out] { outputVideo }'
 
 os.popen(cmdStr)
